d3f6b7383dacabe3f7fbbabbd71fc54e8fa4eb6d
Isabela <isabelahidalgo.2004@gmail.com>
2022-05-25 22:09:13 -0300

    first commit

---
+++ src/com/dio/gof/Teste.java
@@ -0,0 +1,52 @@
+package com.dio.gof;
+
+
+import com.dio.gof.facade.Facade;
+import com.dio.gof.strategy.*;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        // Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        // Strategy
+        System.out.println("\n---------------------------------------------");
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Facade
+        System.out.println("\n---------------------------------------------");
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
+
+
+
+}
